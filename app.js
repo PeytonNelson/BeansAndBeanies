27fac9a65d7aef4054728d6aa8dda011aad79611
PeytonNelson <peytonnelson3@gmail.com>
2022-07-31 17:28:14 -0500

    Set paroller animation for cards

--- app.js
+++ app.js
@@ -4,4 +4,38 @@
 
 
 // }) 
+$('#cardQuality').paroller({
+    factorXs: .2,
+    factorSm: .2,
+    factorMd: .3,
+    factorLg: .2,
+    factorXl: .2,
+    factor: .5,
+    type: 'foreground',
+    direction: 'horizontal',
+    transition: .5
+    });
+$('#cardDedication').paroller({
+    factorXs: -.2,
+    factorSm: -.2,
+    factorMd: -.3,
+    factorLg: -.2,
+    factorXl: -.2,
+    factor: -.5,
+    type: 'foreground',
+    direction: 'horizontal',
+    transition: .5
+    });
+$('#cardCommunity').paroller({
+    factorXs: .2,
+    factorSm: .2,
+    factorMd: .3,
+    factorLg: .2,
+    factorXl: .2,
+    factor: .5,
+    type: 'foreground',
+    direction: 'horizontal',
+    transition: .5
+    });
 
+$('#jumbotronMain').paroller();
